90439b9570a0f6cb851a8f4dc43b1b46844d3e33
Jan <janmarkojukic@hotmail.com>
2024-04-19 13:18:13 +0200

    Project initialization

---
+++ scripts/game_master.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var json = JSON.new()
+var path = "user://data.json"
+
+var leaderboard = {}
+
+func write_save_file(content):
+	var file = FileAccess.open(path, FileAccess.WRITE)
+	file.store_string(json.stringify(content))
+	file.close()
+	file = null
+
+func read_save_file():
+	var file = FileAccess.open(path, FileAccess.READ)
+	var content = json.parse_string(file.get_as_text())
+	return content
+
+func create_new_save_file():
+	var file = FileAccess.open("res://scripts/default_leaderboard.json", FileAccess.READ)
+	var content = json.parse_string(file.get_as_text())
+	leaderboard = content
+	write_save_file(leaderboard)
+	
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if FileAccess.file_exists(path) == false:
+		create_new_save_file()
+	leaderboard = read_save_file()
+	
+	print(leaderboard[2]["score"])
+	leaderboard[2]["score"] += 100
+	write_save_file(leaderboard)
+	
+	print(leaderboard[2]["score"])
+	leaderboard[2]["score"] += 100
+	write_save_file(leaderboard)
