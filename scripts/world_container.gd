40f697c01a41d64f804fe263c42d2e345f9238e5
schultzika <dorian.sulc@yahoo.com>
2024-04-22 19:10:42 +0200

    Merge world gen fixes and updates

--- scripts/world_container.gd
+++ scripts/world_container.gd
@@ -5,7 +5,7 @@ const BLOCKER = 999
 const FLOOR_1 = 1
 const FLOOR_2 = 2
 const FLOOR_3 = 3
-const FLOOR_4 = 4
+const FLOOR_DOOR = 4
 const OCCLUDER = 5
 const WINDOW = 6
 const SPACE_DEFAULT = 7
@@ -17,6 +17,7 @@ const SPACE_5 = 12
 const SPACE_6 = 13
 const SPACE_7 = 14
 const SPACE_8 = 15
+const OCCLUDER_2 = 16
 
 #tile id to Atlas coordinates map
 const TILE_COORDS = {
@@ -24,7 +25,7 @@ const TILE_COORDS = {
 	FLOOR_1: Vector2i(0, 0),
 	FLOOR_2: Vector2i(1, 0),
 	FLOOR_3: Vector2i(2, 0),
-	FLOOR_4: Vector2i(3, 0),
+	FLOOR_DOOR: Vector2i(3, 0),
 	OCCLUDER: Vector2i(0, 2),
 	WINDOW: Vector2i(0, 9),
 	SPACE_DEFAULT: Vector2i(5, 9),
@@ -36,6 +37,7 @@ const TILE_COORDS = {
 	SPACE_6: Vector2i(7, 9),
 	SPACE_7: Vector2i(8, 9),
 	SPACE_8: Vector2i(9, 9),
+	OCCLUDER_2: Vector2i(4, 3),
 }
 var COORDS_TILE = {} #inverse to the above, populated in constructor
 
@@ -64,15 +66,24 @@ const DIRY8 = [-1, -1, 0, 1, 1, 1, 0, -1]
 const DLTX = [1, 1, -1, -1]
 const DLTY = [1, -1, 1, -1]
 
+#door patterns
+const DOOR_PATTERN_DIR = [0, 1, 2, 6, 7]
+const DOOR_PATTERNS = [[true, true, false, false, true],
+						[true, false, false, false, true],
+						[true, true, false, false, false]]
+
 #references - set up in constructor
 var tile_map = null
 var rng = null
 
 #world data
+var loaded_chunk = null
 var generated_chunks = {}
 var room_ids = {}
 var room_centers = {}
 var hallway_coords = {}
+var graph = {}
+var tile_map_backend = {}
 
 func _init(new_tile_map):
 	tile_map = new_tile_map
@@ -80,22 +91,21 @@ func _init(new_tile_map):
 	rng.randomize()
 	for k in TILE_COORDS:
 		COORDS_TILE[TILE_COORDS[k]] = k
-	
+
 	for i in range(0, len(FLOOR_TILES)):
 		for j in range(0, FLOOR_CHANCES[i]):
 			FLOOR_CHANCE_ARRAY.append(FLOOR_TILES[i])
 	for i in range(0, len(SPACE_TILES)):
 		for j in range(0, SPACE_CHANCES[i]):
 			SPACE_CHANCE_ARRAY.append(SPACE_TILES[i])
-	print(FLOOR_CHANCE_ARRAY)
-	
+
 
 func _ready():
 	pass
 
 func choose_floor():
 	return FLOOR_CHANCE_ARRAY[rng.randi_range(0, 99)]
-	
+
 func choose_space():
 	return SPACE_CHANCE_ARRAY[rng.randi_range(0, 99)]
 
@@ -108,18 +118,18 @@ func generate_room(x_center, y_center):
 	for i in range(y_center-0, y_center+0+1):
 		for j in range(x_center-0, x_center+0+1):
 			room_coords[Vector2i(j, i)] = choose_floor()
-	
+
 	var room_cnt = rng.randi_range(1, 4)
 	var overlapped = false
 	for i in range(0, room_cnt):
 		var key = room_coords.keys().pick_random()
 		var kx = key[0]
 		var ky = key[1]
-		
+
 		var dir = rng.randi_range(0, 3)
 		var ylen = rng.randi_range(4, 8)
 		var xlen = rng.randi_range(4, 8)
-		
+
 		var room_part = {}
 		for y in range(0, ylen):
 			var ny = ky + y*DLTY[dir]
@@ -129,15 +139,14 @@ func generate_room(x_center, y_center):
 		
 		var overlap = false
 		for k in room_part:
-			var tile = tile_map.get_cell_atlas_coords(0, k)
-			if tile[0] != -1:
+			if k in tile_map_backend:
 				overlap = true
 				break
-		
+
 		if not overlap:
 			overlapped = true
 			room_coords.merge(room_part)
-	
+
 	if not overlapped:
 		for k in room_coords:
 			room_coords = { Vector2i(x_center, y_center): BLOCKER }
@@ -156,48 +165,99 @@ func extend_room(room, tile_id):
 	room.merge(ext)
 	return room
 
-func add_tile(tile_id, x, y):
+func show_tile(tile_id, x, y):
 	#Adds a tile to the world
 	#tile_id = one of the constants on top of this file like FLOOR_1 etc.
 	#x, y = tile map coords in the world
 	tile_map.set_cell(0, Vector2i(x, y), 0, TILE_COORDS[tile_id], 0)
+
+func show_chunk(x_chunk, y_chunk):
+	var x_center = x_chunk * 25 + 12
+	var y_center = y_chunk * 25 + 12
 	
-func show_room(room):
-	#Sends room data to tile_map
+	tile_map.clear()
+	for i in range(y_center-40, y_center+40+1):
+		for j in range(x_center-40, x_center+40+1):
+			if Vector2i(j, i) in tile_map_backend:
+				show_tile(tile_map_backend[Vector2i(j, i)], j, i)
+	
+	loaded_chunk = Vector2i(x_chunk, y_chunk)
+
+func save_room(room):
 	for k in room.keys():
 		if room[k] == BLOCKER:
-			var tile = tile_map.get_cell_atlas_coords(0, k)
-			if tile[0] != -1:
+			if k in tile_map_backend:
 				continue
-		add_tile(room[k], k[0], k[1])
-		
-func generate_hallway(from_x, from_y, to_x, to_y):
+		tile_map_backend[k] = room[k]
+
+func generate_hallway(from_x, from_y, to_x, to_y, from_id, to_id):
 	#Generates a hallway between 2 coordinates
 	#Returned dict:
 	#	key: Vector2i -> representing x,y coords in global space
 	#	value: tile_id constant (like FLOOR_1 etc.)
 	var dx = sign(to_x - from_x)
 	var dy = sign(to_y - from_y)
-	
+
 	var cx = from_x
 	var cy = from_y
+	var hallway = []
 	while true:
 		if cx == to_x and cy == to_y:
 			break
 		
-		add_tile(choose_floor(), cx, cy)
+		tile_map_backend[Vector2i(cx, cy)] = choose_floor()
+		hallway.append(Vector2i(cx, cy))
+
 		for d in range(0, 8):
 			var nx = cx + DIRX8[d]
 			var ny = cy + DIRY8[d]
-			var tile = tile_map.get_cell_atlas_coords(0, Vector2i(nx, ny))
-			if tile[0] == -1 or COORDS_TILE[tile] == BLOCKER:
-				add_tile(OCCLUDER, nx, ny)
-		
+			if Vector2i(nx, ny) not in tile_map_backend:
+				tile_map_backend[Vector2i(nx, ny)] = OCCLUDER
+			elif tile_map_backend[Vector2i(nx, ny)] == BLOCKER:
+				tile_map_backend[Vector2i(nx, ny)] = OCCLUDER
+
 		if cx != to_x:
 			cx += dx
 		else:
 			cy += dy
-		
+
+	for h in hallway:
+		cx = h[0]
+		cy = h[1]
+		var found = false
+		for p in DOOR_PATTERNS:
+			for k in range(0, 4):
+				var all_true = true
+				for i in range(0, 5):
+					var nx = cx + DIRX8[(DOOR_PATTERN_DIR[i] + k*2)%8]
+					var ny = cy + DIRY8[(DOOR_PATTERN_DIR[i] + k*2)%8]
+
+					if (tile_map_backend[Vector2i(nx, ny)] == OCCLUDER or tile_map_backend[Vector2i(nx, ny)] == OCCLUDER_2) == p[i]:
+						all_true = false
+						break
+				if all_true:
+					#manual check if 2 in front is occluder - that's a weird hallway pattern
+					var nx = cx + 2*DIRX4[k]
+					var ny = cy + 2*DIRY4[k]
+
+					if tile_map_backend[Vector2i(nx, ny)] == OCCLUDER or tile_map_backend[Vector2i(nx, ny)] == OCCLUDER_2:
+						continue
+					else:
+						#manual check if left and right from the one 2 in front are both occluders
+						var nx1 = nx + DIRX4[(k+1)%4]
+						var ny1 = ny + DIRY4[(k+1)%4]
+						var nx2 = nx + DIRX4[(k+3)%4]
+						var ny2 = ny + DIRY4[(k+3)%4]
+
+						if (tile_map_backend[Vector2i(nx1, ny1)] == OCCLUDER or tile_map_backend[Vector2i(nx1, ny1)] == OCCLUDER_2) and \
+							(tile_map_backend[Vector2i(nx2, ny2)] == OCCLUDER or tile_map_backend[Vector2i(nx2, ny2)] == OCCLUDER_2):
+								continue
+
+					found = true
+					tile_map_backend[Vector2i(cx, cy)] = FLOOR_DOOR
+					break
+			if found:
+				break
 
 var first_gen = true
 func generate_env(x_center, y_center):
@@ -210,27 +270,22 @@ func generate_env(x_center, y_center):
 		x_center = x_chunk * 25 + 12
 		y_center = y_chunk * 25 + 12
 	else:
+		if loaded_chunk != Vector2i(x_chunk, y_chunk):
+			show_chunk(x_chunk, y_chunk)
 		return
-	
-	var free_tiles = {}
-	for i in range(y_center-50, y_center+50+1):
-		for j in range(x_center-50, x_center+50+1):
-			var tile = tile_map.get_cell_atlas_coords(0, Vector2i(j, i))
-			if tile[0] == -1:
-				free_tiles[Vector2i(j, i)] = null
-	
+
 	#take the starting room from engine
 	var new_rooms = []
 	if first_gen:
 		first_gen = false
-		
+
 		var room = {}
 		for i in range(y_center-50, y_center+50+1):
 			for j in range(x_center-50, x_center+50+1):
 				var tile = tile_map.get_cell_atlas_coords(0, Vector2i(j, i))
 				if tile[0] != -1:
 					room[Vector2i(j, i)] = COORDS_TILE[tile]
-		
+
 		var current_room = len(room_centers)
 		var sum_x = 0
 		var sum_y = 0
@@ -243,14 +298,18 @@ func generate_env(x_center, y_center):
 				count += 1
 		room_centers[current_room] = Vector2i(round(sum_x / count), round(sum_y / count))
 		new_rooms.append(current_room)
-		
+
 		room = extend_room(room, OCCLUDER)
 		room = extend_room(room, BLOCKER)
-		for k in room:
-			free_tiles.erase(k)
-		show_room(room)
-	
-	#generate other rooms 
+		save_room(room)
+
+	var free_tiles = {}
+	for i in range(y_center-50, y_center+50+1):
+		for j in range(x_center-50, x_center+50+1):
+			if Vector2i(j, i) not in tile_map_backend:
+				free_tiles[Vector2i(j, i)] = null
+
+	#generate other rooms
 	while len(free_tiles.keys()) > 0:
 		var room_anchor = free_tiles.keys().pick_random()
 		var room = generate_room(room_anchor[0], room_anchor[1])
@@ -267,7 +326,7 @@ func generate_env(x_center, y_center):
 					count += 1
 			room_centers[current_room] = Vector2i(round(sum_x / count), round(sum_y / count))
 			new_rooms.append(current_room)
-		
+			
 		if len(room) > 10:
 			room = extend_room(room, OCCLUDER)
 		else:
@@ -275,145 +334,158 @@ func generate_env(x_center, y_center):
 		room = extend_room(room, BLOCKER)
 		for k in room:
 			free_tiles.erase(k)
-		show_room(room)
-	
+		save_room(room)
+
 	#generate hallways
-	var graph = {}
 	for i in range(0, len(room_centers)):
-		graph[i] = {}
-	
+		if i not in graph:
+			graph[i] = {}
+
 	for nr in new_rooms:
 		var nrx = room_centers[nr][0]
 		var nry = room_centers[nr][1]
 		var close_rooms = {}
-		for i in range(nry-20, nry+20+1):
-			for j in range(nrx-20, nrx+20+1):
+		for i in range(nry-15, nry+15+1):
+			for j in range(nrx-15, nrx+15+1):
 				if Vector2i(j, i) in room_ids:
 					if nr != room_ids[Vector2i(j, i)]:
 						close_rooms[room_ids[Vector2i(j, i)]] = null
-		
+
 		var dists = []
 		for next in close_rooms:
 			var dist = abs(room_centers[nr][0] - room_centers[next][0]) + \
 					   abs(room_centers[nr][1] - room_centers[next][1])
 			dists.append(Vector2i(dist, next))
 		dists.sort()
-		
-		var halls = 2
+
+		var halls = 0
+		if len(dists) >= 1:
+			halls = 1
+		if len(dists) >= 2:
+			halls = 2
 		if len(dists) >= 3:
 			if rng.randf() >= 0.5:
 				halls = 3
-		
+
 		for h in range(0, halls):
 			if dists[h][1] in graph[nr]:
 				continue
 			graph[nr][dists[h][1]] = null
 			graph[dists[h][1]][nr] = null
 			generate_hallway(room_centers[nr][0], room_centers[nr][1], \
-							 room_centers[dists[h][1]][0], room_centers[dists[h][1]][1])
-		
+							 room_centers[dists[h][1]][0], room_centers[dists[h][1]][1], \
+							 nr, dists[h][1])
+
 	#add space tiles outside
 	for i in range(y_center-50, y_center+50+1):
 		for j in range(x_center-50, x_center+50+1):
-			var tile = tile_map.get_cell_atlas_coords(0, Vector2i(j, i))
-			if tile[0] == -1:
+			if Vector2i(j, i) not in tile_map_backend:
 				continue
-			if COORDS_TILE[tile] == BLOCKER:
-				add_tile(choose_space(), j, i)
-	
+			if tile_map_backend[Vector2i(j, i)] == BLOCKER:
+				tile_map_backend[Vector2i(j, i)] = choose_space()
+
 	#add windows - x axis
+	var occluder_coords = {}
 	for i in range(y_center-50, y_center+50+1):
 		for j in range(x_center-50, x_center+50+1):
 			#5 occluders in row
 			var found = true
 			for k in range(-2, 3):
 				var nx = j+k
-				var tile = tile_map.get_cell_atlas_coords(0, Vector2i(nx, i))
-				if tile[0] == -1:
+				if Vector2i(nx, i) not in tile_map_backend:
 					found = false
 					break
-				if COORDS_TILE[tile] != OCCLUDER:
+				if tile_map_backend[Vector2i(nx, i)] != OCCLUDER:
 					found = false
 					break
+				occluder_coords[Vector2i(nx, i)] = null
 			if not found:
 				continue
-			
+
 			#3 spaces next to occluder on each side are not occluders
 			found = true
 			for k in range(-1, 2):
 				var nx = j+k
-				var tile = tile_map.get_cell_atlas_coords(0, Vector2i(nx, i-1))
-				if tile[0] == -1:
+				if Vector2i(nx, i-1) not in tile_map_backend:
 					found = false
 					break
-				if COORDS_TILE[tile] == OCCLUDER:
+				if tile_map_backend[Vector2i(nx, i-1)] == OCCLUDER or tile_map_backend[Vector2i(nx, i-1)] == OCCLUDER_2:
 					found = false
 					break
+				occluder_coords[Vector2i(nx, i-1)] = null
 			if not found:
 				continue
-			
+
 			found = true
 			for k in range(-1, 2):
 				var nx = j+k
-				var tile = tile_map.get_cell_atlas_coords(0, Vector2i(nx, i+1))
-				if tile[0] == -1:
+				if Vector2i(nx, i+1) not in tile_map_backend:
 					found = false
 					break
-				if COORDS_TILE[tile] == OCCLUDER:
+				if tile_map_backend[Vector2i(nx, i+1)] == OCCLUDER or tile_map_backend[Vector2i(nx, i+1)] == OCCLUDER_2:
 					found = false
 					break
+				occluder_coords[Vector2i(nx, i+1)] = null
 			if not found:
 				continue
-				
+
 			if rng.randf() <= WINDOW_CHANCE:
 				for k in range(-1, 2):
 					var nx = j+k
-					add_tile(WINDOW, nx, i)
-					
+					tile_map_backend[Vector2i(nx, i)] = WINDOW
+
 	#add windows - y axis
 	for i in range(y_center-50, y_center+50+1):
 		for j in range(x_center-50, x_center+50+1):
 			var found = true
 			for k in range(-2, 3):
 				var ny = i+k
-				var tile = tile_map.get_cell_atlas_coords(0, Vector2i(j, ny))
-				if tile[0] == -1:
+				if Vector2i(j, ny) not in tile_map_backend:
 					found = false
 					break
-				if COORDS_TILE[tile] != OCCLUDER:
+				if tile_map_backend[Vector2i(j, ny)] != OCCLUDER:
 					found = false
 					break
+				occluder_coords[Vector2i(j, ny)] = null
 			if not found:
 				continue
-			
+
 			#y-axis - 3 spaces next to occluder on each side are not occluders
 			found = true
 			for k in range(-1, 2):
 				var ny = i+k
-				var tile = tile_map.get_cell_atlas_coords(0, Vector2i(j-1, ny))
-				if tile[0] == -1:
+				if Vector2i(j-1, ny) not in tile_map_backend:
 					found = false
 					break
-				if COORDS_TILE[tile] == OCCLUDER:
+				if tile_map_backend[Vector2i(j-1, ny)] == OCCLUDER or tile_map_backend[Vector2i(j-1, ny)] == OCCLUDER_2:
 					found = false
 					break
+				occluder_coords[Vector2i(j-1, ny)] = null
 			if not found:
 				continue
-				
+
 			found = true
 			for k in range(-1, 2):
 				var ny = i+k
-				var tile = tile_map.get_cell_atlas_coords(0, Vector2i(j+1, ny))
-				if tile[0] == -1:
+				if Vector2i(j+1, ny) not in tile_map_backend:
 					found = false
 					break
-				if COORDS_TILE[tile] == OCCLUDER:
+				if tile_map_backend[Vector2i(j+1, ny)] == OCCLUDER or tile_map_backend[Vector2i(j+1, ny)] == OCCLUDER_2:
 					found = false
 					break
+				occluder_coords[Vector2i(j+1, ny)] = null
 			if not found:
 				continue
-			
+
 			if rng.randf() <= WINDOW_CHANCE:
 				for k in range(-1, 2):
 					var ny = i+k
-					add_tile(WINDOW, j, ny)
+					tile_map_backend[Vector2i(j, ny)] = WINDOW
+
+	for k in occluder_coords:
+		if tile_map_backend[k] == OCCLUDER:
+			tile_map_backend[k] = OCCLUDER_2
+	
+	if loaded_chunk != Vector2i(x_chunk, y_chunk):
+		show_chunk(x_chunk, y_chunk)
+		
